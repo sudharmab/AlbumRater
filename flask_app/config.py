2dd60fa0e54bf902f1db040a2d5941e4a69e556a
sudharmab <114265359+sudharmab@users.noreply.github.com>
2025-10-16 14:39:27 -0400

    Fix MongoDB host configuration in config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -2,7 +2,6 @@ import certifi
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-MONGODB_HOST = os.environ.get("MONGODB_HOST") + certifi.where()
-
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
 
 # testing to make sure this edit is made in nico branch
